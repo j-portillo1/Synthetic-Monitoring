ef4de6ec0ab9f9f2af535b4b6eac282ae9b10da5
J-portillo1 <portillo1@simmons.edu>
2024-04-09 22:19:23 -0400

    recent expose metrics

--- Monitoring/main.py
+++ Monitoring/main.py
@@ -3,57 +3,48 @@ import pingparsing
 from prometheus_client import start_http_server, Gauge
 
 #website = input("Enter Website URL: ")
-    
-packet_loss_count = Gauge("packet_loss_count", "Packet loss")
-packet_loss_rate = Gauge("packet_loss_rate","Packet Loss Rate")
-packet_transmit = Gauge("packet_transmit", "Packets Trasmitted")
-packet_receive = Gauge("packet_recieve", "Packets Recieved")
-rtt_min = Gauge("rtt_min", "Round trip time minimum")
-rtt_avg = Gauge("rtt_avg", "Round trip time average")
-rtt_max = Gauge("rtt_max", "Round trip time maximum")
+#intialize metric types     
+#packet_loss_count = Gauge("packet_loss_count", "Packet loss")
+#packet_loss_rate = Gauge("packet_loss_rate","Packet Loss Rate")
+packet_transmit = Gauge('packet_transmit', "Packets Trasmitted") #,['host'])
+packet_receive = Gauge('packet_receive', "Packets Recieved")#,['host'])
+rtt_min = Gauge('rtt_min', "Round trip time minimum")#,['host'])
+rtt_avg = Gauge('rtt_avg', "Round trip time average")#,['host'])
+rtt_max = Gauge('rtt_max', "Round trip time maximum")#,['host'])
 
 def ping():
-    start_http_server(9090)
-    print("Prometheus HTTP server started on port 9090")
+    start_http_server(8989)
+    print("Prometheus HTTP server started on port 8989")
     ping_parser = pingparsing.PingParsing()
     transmitter = pingparsing.PingTransmitter()
     transmitter.destination = "www.google.com"
     transmitter.count = 10
     result = transmitter.ping()
-    ping_result = ping_parser.parse(result)
-    #.as_dict()
+    ping_result = ping_parser.parse(result).as_dict()
+    print(json.dumps(ping_parser.parse(result).as_dict(), indent=4))
     print(ping_result)
-    #print(json.dumps(ping_parser.parse(result).as_dict(), indent=4))
 
-    if ping_result.packet_loss_count is not None:
-    #print(packet_loss_count.set(ping_result["packet_loss_count"]))
-        print(packet_loss_count.set(ping_result.packet_loss_count))
 
-    #packet_loss_rate.set(ping_result["packet_loss_rate"])
-    if ping_result.packet_loss_rate is not None:
-        print(packet_loss_count.set(ping_result.packet_loss_rate))
-
-    if ping_result.packet_transmit is not None:
-        print(packet_transmit.set(ping_result.packet_transmit))
+    rtt_avg = ping_result['rtt_avg']
+    rtt_min = ping_result['rtt_min']
+    rtt_max = ping_result['rtt_max']
+    packets_received = ping_result['packet_receive']
+    packets_transmited = ping_result['packet_transmit']
     
-    if ping_result.packet_receive is not None:
-        print(packet_receive.set(ping_result.packet_receive))
-
-    if ping_result.rtt_min is not None:
-        print(rtt_min.set(ping_result.rtt_min))
-
-    if ping_result.rtt_avg is not None:
-        print(rtt_avg.set(ping_result.rtt_avg))
-
-    if ping_result.rtt_max is not None: 
-        print(rtt_max.set(ping_result.rtt_max))
+    print(rtt_avg)
+    print(rtt_min)
+    print(rtt_max)
+    print(packets_received)
+    print(packets_transmited)
+    
+    #set metric values 
+    rtt_min.set(rtt_min)
+    rtt_avg.set(rtt_avg)
+    rtt_max.set(rtt_max)
+    packet_receive.set(packets_received)
+    packet_transmit.set(packets_transmited)
     
 
-    
 
-    #print(json.dumps(ping_parser.parse(result).as_dict(), indent=4))
-
-
-
-print("[extract ping statistics]")
+#print("[extract ping statistics]")
 ping()
