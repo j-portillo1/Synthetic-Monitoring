64a7a6851875e3c645dec5be064bd812ee4e94b9
J-portillo1 <portillo1@simmons.edu>
2024-04-11 00:57:14 -0400

    ping metrics using yaml file

--- Monitoring/main.py
+++ Monitoring/main.py
@@ -2,8 +2,24 @@ import json
 import pingparsing 
 from prometheus_client import start_http_server, Gauge
 import time
+import yaml
 
-#website = input("Enter Website URL: ")
+#check and read yaml file 
+def read_yaml(): 
+    path = str(input("Please Enter file path: "))
+
+    try:
+        with open(path, 'r') as file:
+            yaml_data = yaml.safe_load(file)
+        return yaml_data
+
+    except FileNotFoundError:
+        print("File not Found")
+
+    except yaml.YAMLError as error:
+        print("Error: Invalid YAML format.")
+        print(error)
+
 #intialize metric types     
 packet_loss_count = Gauge("packet_loss_count", "Packet loss")
 packet_loss_rate = Gauge("packet_loss_rate","Packet Loss Rate")
@@ -13,20 +29,20 @@ rtt_min = Gauge('rtt_min', "Round trip time minimum")#,['host'])
 rtt_avg = Gauge('rtt_avg', "Round trip time average")#,['host'])
 rtt_max = Gauge('rtt_max', "Round trip time maximum")#,['host'])
 
-def ping():
-    start_http_server(8989)
+def ping(http_port, website, duration):
+    start_http_server(http_port)
     print("Prometheus HTTP server started on port 8989")
     while True: 
         ping_parser = pingparsing.PingParsing()
         transmitter = pingparsing.PingTransmitter()
-        transmitter.destination = "www.google.com"
-        transmitter.count = 10
+        transmitter.destination = website
+        transmitter.count = duration
         result = transmitter.ping()
         ping_result = ping_parser.parse(result).as_dict()
         print(json.dumps(ping_parser.parse(result).as_dict(), indent=4))
         print(ping_result)
 
-        
+        #creating metric variables 
         latency = ping_result['rtt_avg']
         latency_min = ping_result['rtt_min']
         latency_max = ping_result['rtt_max']
@@ -34,14 +50,6 @@ def ping():
         packets_transmited = ping_result['packet_transmit']
         packets_lost = ping_result['packet_loss_count']
         packets_lost_rt = ping_result['packet_loss_rate']
-        
-        print(latency)
-        print(latency_min)
-        print(latency_max)
-        print(packets_received)
-        print(packets_transmited)
-        print(packets_lost)
-        print(packets_lost_rt)
         
         #set metric values 
         rtt_min.set(latency)
@@ -54,6 +62,23 @@ def ping():
         time.sleep(3)
         
 
+def main():
+    yaml_data = read_yaml()
+    if yaml_data is not None:
+            for website_info in yaml_data['monitor_targets']:
+                website = website_info['website']
+                duration = website_info['duration']
+                http_port = website_info['port']
 
-#print("[extract ping statistics]")
-ping()
+                metrics = ping(http_port, website,duration)
+                if metrics is not None:
+                    print("Metrics collected successfully")
+                else:
+                    print("Failed to collect metrics")
+
+    else:
+        print("Error with YAML file")
+
+main()
+
+
